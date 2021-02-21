c118d1b4db9942853c82ee21f8a3b81480248387
Trương Hữu Tiến <tienlk25@gmai.com>
2021-02-21 22:10:53 +0700

    upadate ui bot

--- src/containers/Bot/ListBot/index.jsx
+++ src/containers/Bot/ListBot/index.jsx
@@ -7,35 +7,6 @@ function ListBot(props) {
   const { bots } = props;
   return (
     <div>
-      {/*{bots?.map((bot) => {
-        return (
-        <Paper className={classes.root} elevation={5}>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Avatar className={classes.rounded} variant="rounded" />
-            </Grid>
-            <Grid item>
-              <Grid xs>
-                <Typography variant="h6" gutterBottom>
-                  {bot.name}
-                </Typography>
-              </Grid>
-               <Grid container className={classes.gridBody} spacing={10} item>
-                <Grid item>
-                  <Typography>intent: 3</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>action: 4</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>entity: 5</Typography>
-                </Grid>
-              </Grid> 
-            </Grid>
-          </Grid>
-        </Paper>
-      )})} */}
-
       <Grid container spacing={3}>
         {bots?.map((bot) => {
           return (
