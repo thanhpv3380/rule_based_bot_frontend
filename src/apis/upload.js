bedd209ae0989cf22de99f14ca0a6d322efb5e8a
Henry <thanhpv3380@gmail.com>
2021-06-23 19:29:14 +0700

    fix api upload image

--- src/apis/upload.js
+++ src/apis/upload.js
@@ -1,14 +1,14 @@
-import API from './authApi';
+import axios from 'axios';
 
 export async function uploadFile({ formData }) {
   try {
-    const response = await API({
+    const response = await axios({
       method: 'POST',
       url: `${process.env.REACT_APP_UPLOAD_FILE_DOMAIN}/api/v1/uploads/file`,
       data: formData,
       headers: { 'Content-Type': 'multipart/form-data' },
     });
-    return response;
+    return response.data;
   } catch (error) {
     return error.response;
   }
